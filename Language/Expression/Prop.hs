f66385a30c1c164524454f91a2cf28f393d54e32
Bradley Hardy <bradleyhardy@live.com>
2017-09-21 14:16:01 +0100

    Try some new things with forall binders

--- Language/Expression/Prop.hs
+++ Language/Expression/Prop.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ConstraintKinds       #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE FlexibleContexts      #-}
@@ -36,7 +37,7 @@ module Language.Expression.Prop where
   -- , LogicOp(..)
   -- ) where
 
-import           Data.Monoid                (All (..), Any (..))
+import           Data.Monoid (All(..))
 
 import           Data.Functor.Compose
 import           Data.Functor.Identity
@@ -49,152 +50,103 @@ import           Language.Expression.Util
 import           Language.Expression.Scope
 import qualified Language.Expression.Prop.Simple as S
 
-
-type Prop = SFree BPOp
-
 --------------------------------------------------------------------------------
---  DSL
+--  Propositions
 --------------------------------------------------------------------------------
+
+-- data Prop expr v a where
+--   PropExpr :: Scope g expr v a -> Prop expr v a
+
+-- type Prop = SFree PropOp
+
+-- --------------------------------------------------------------------------------
+-- --  DSL
+-- --------------------------------------------------------------------------------
 
 -- infixl 3 *&&
 -- infixl 2 *||
 -- infixr 1 *->
 -- infix 1 *<->
 
--- | Lift an expression into the land of propositions.
-expr :: expr a -> Prop expr a
-expr = SPure
+-- -- | Lift an expression into the land of propositions.
+-- expr :: expr a -> Prop expr a
+-- expr = SPure
 
-plit :: Bool -> Prop expr Bool
-plit = SWrap . BPSimple . S.LogLit
+-- plit :: Bool -> Prop expr Bool
+-- plit = SWrap . PopSimple . S.LogLit
 
-pnot :: Prop expr Bool -> Prop expr Bool
-pnot = SWrap . BPSimple . S.LogNot
+-- pnot :: Prop expr Bool -> Prop expr Bool
+-- pnot = SWrap . PopSimple . S.LogNot
 
-(*&&) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
-(*&&) = SWrap ... BPSimple ... S.LogAnd
+-- (*&&) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
+-- (*&&) = SWrap ... PopSimple ... S.LogAnd
 
-(*||) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
-(*||) = SWrap ... BPSimple ... S.LogOr
+-- (*||) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
+-- (*||) = SWrap ... PopSimple ... S.LogOr
 
-(*->) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
-(*->) = SWrap ... BPSimple ... S.LogImpl
+-- (*->) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
+-- (*->) = SWrap ... PopSimple ... S.LogImpl
 
-(*<->) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
-(*<->) = SWrap ... BPSimple ... S.LogEquiv
-
-
-data AbsExpr a expr b where
-  AbsExpr :: expr b -> AbsExpr a expr b
-  AbsVar :: String -> AbsExpr a expr a
-
-instance HFunctor (AbsExpr a)
-instance HTraversable (AbsExpr a) where
-  htraverse f = \case
-    AbsExpr x -> AbsExpr <$> f x
-    AbsVar s -> pure (AbsVar s)
+-- (*<->) :: Prop expr Bool -> Prop expr Bool -> Prop expr Bool
+-- (*<->) = SWrap ... PopSimple ... S.LogEquiv
 
 
-absBinder1 :: (Finite a) => Prop (AbsExpr a expr) b -> Scope (BPBinderEnumerable a) Prop expr b
-absBinder1
-  = Scope
-  . hmap (\case AbsExpr x -> F (expr x)
-                AbsVar _ -> B BPBinderEnumerable
-         )
-
-data HCompose h h' t a where
-  HCompose :: h (h' t) a -> HCompose h h' t a
-
-absBinder2 :: Prop (Scope (BPBinderEnumerable a) expr v) b -> Scope (BPBinderEnumerable a) (HCompose Prop expr) v b
-absBinder2 = Scope . hmap _
-
--- abstract f = Scope . hmap (\y -> maybe ((hpure . hpure) y) B $ f y)
-
-
-pforall :: String -> Prop (Scope (BPBinderEnumerable a) expr v) b -> Prop (expr v) Bool
-pforall nm f = undefined
-
+-- pforall :: (HFunctor expr) => Prop (Scope (Enumerable a) expr v) Bool -> Prop (expr v) Bool
+-- pforall = SWrap . PopForall . Scoped . _
 
 --------------------------------------------------------------------------------
 --  Operators
 --------------------------------------------------------------------------------
 
-data BPBinderEnumerable a b where
-  BPBinderEnumerable :: Finite a => BPBinderEnumerable a a
+data Enumerable a b where
+  Enumerable :: (SymWord a, Finite a) => Enumerable a a
 
-data BPBinderType = BForall | BExists
-  deriving (Eq, Ord, Show, Read)
+data PropOp s t a where
+  PopForall :: s (Enumerable a) Bool -> PropOp s t Bool
+  PopSimple :: S.LogicOp t a -> PropOp s t a
 
+--------------------------------------------------------------------------------
+--  Instances
+--------------------------------------------------------------------------------
 
-data BPBinder s t b where
-  BPBind :: BPBinderType -> s (Compose t (BPBinderEnumerable a)) Bool -> BPBinder s t Bool
-
-
-instance HDuofunctor BPBinder
-instance HDuotraversable BPBinder where
+instance HDuofunctor PropOp
+instance HDuotraversable PropOp where
   hduotraverse g f = \case
-    BPBind bt y -> BPBind bt <$> g (\(Compose z) -> Compose <$> f z) y
+    PopForall x -> PopForall <$> g pure x
+    PopSimple x -> PopSimple <$> htraverse f x
+
+instance HFunctor s => HFunctor (PropOp s) where
+  hmap = hduomapSecond
+instance HTraversable s => HTraversable (PropOp s) where
+  htraverse = hduotraverseSecond
+
+instance HDuofoldableAt Identity PropOp where
+  hduofoldMap =
+    implHduofoldMap $ \g -> \case
+      PopForall x ->
+        fmap getAll .
+        foldMap @[] (fmap All . g id) .
+        htraverse (\Enumerable -> Identity <$> enumAll) $
+        x
+      PopSimple x -> hfold x
 
 
-instance HDuofoldableAt Identity BPBinder where
-  hduofoldMap = implHduofoldMap $ \g -> \case
-    BPBind bt y ->
-      Identity $ foldBT (runIdentity . g id) $
-      htraverse (\(Compose (Identity BPBinderEnumerable)) ->
-                   Identity <$> enumAll) y
-      where
-        foldBT :: (a -> Bool) -> [a] -> Bool
-        foldBT f = case bt of
-          BForall -> getAll . foldMap (All . f)
-          BExists -> getAny . foldMap (Any . f)
-
-data SBVProp a where
-  SBVEnumerable :: SymWord a => String -> SBVProp (BPBinderEnumerable a a)
-  SBVTerm :: SBV a -> SBVProp a
-
-
-instance HDuofoldableAt (Compose Symbolic SBVProp) BPBinder where
+instance HDuofoldableAt (Compose Symbolic SBV) PropOp where
   hduofoldMap = implHduofoldMapCompose $ \g -> \case
-    BPBind bt y ->
-      g getCompose $
-      hmap (\(Compose (SBVEnumerable nm)) ->
-              let sym = case bt of
-                    BForall -> forall nm
-                    BExists -> exists nm
-              in Compose (SBVTerm <$> sym)
-           ) y
-
-
-data BPOp s t a where
-  BPBinder :: BPBinder s t Bool -> BPOp s t Bool
-  BPSimple :: S.LogicOp t a -> BPOp s t a
-
-instance HDuofunctor BPOp
-instance HDuotraversable BPOp where
-  hduotraverse g f = \case
-    BPBinder x -> BPBinder <$> hduotraverse g f x
-    BPSimple x -> BPSimple <$> htraverse f x
-
-instance HDuofoldableAt Identity BPOp where
-  hduofoldMap g f = \case
-    BPBinder x -> hduofoldMap g f x
-    BPSimple x -> hfoldMap f x
-
-
-instance HFoldableAt (Compose Symbolic SBVProp) S.LogicOp where
-  hfoldMap = implHfoldMapCompose $ \x ->
-    let x' = hmap (\(SBVTerm y) -> y) x
-    in return . SBVTerm $ hfold x'
-
-
-instance HDuofoldableAt (Compose Symbolic SBVProp) BPOp where
-  hduofoldMap g f = \case
-    BPBinder x -> hduofoldMap g f x
-    BPSimple x -> hfoldMap f x
-
+    PopForall x -> g (\Enumerable -> forall_) x
+    PopSimple x -> pure (hfold x)
 
 
 class (Enum a, Bounded a) => Finite a where
   enumAll :: [a]
   enumAll = [minBound .. maxBound]
-instance (Enum a, Bounded a) => Finite a
+
+--------------------------------------------------------------------------------
+--  Util
+--------------------------------------------------------------------------------
+
+-- rescope :: (HFunctor h) => Scope g h f a -> Scope g (SFree h') (h f) a
+-- rescope (Scope x) = Scope (SPure _)
+
+
+-- x :: h (BV g (h f)) a
